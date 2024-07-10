144da15a7a48b43954dfcf31a260233b69af70c0
Neves Tuan <neves.pham@gmail.com>
2024-07-10 16:32:34 +1000

    Version: 0.0.34

--- startApi.sh
+++ startApi.sh
@@ -1,10 +1,31 @@
+#!/bin/bash
+
+# Checkout to the main branch and reset to the latest from origin
 git checkout main
 git fetch origin
 git reset --hard origin/main
+
+# Install dependencies
 yarn
+
+# Start the database
 ./startDB.sh
-if ! test -f .env ; then 
-    rm -rf .env
+
+# Check if .env file exists
+if test -f .env; then 
+    # Extract npm_package_version line from .env.prod
+    npm_package_version=$(grep 'npm_package_version' .env.prod)
+    
+    # Replace the npm_package_version line in .env
+    if grep -q 'npm_package_version' .env; then
+        sed -i "s/npm_package_version=.*/$npm_package_version/" .env
+    else
+        echo "$npm_package_version" >> .env
+    fi
+else
+    # If .env does not exist, copy .env.prod to .env
     cp .env.prod .env
 fi
-HTTPS=true  node -r newrelic ./build/index.js
+
+# Start the server with HTTPS enabled
+HTTPS=true node -r newrelic ./build/index.js
