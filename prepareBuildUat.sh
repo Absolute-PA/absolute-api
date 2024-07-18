db9a901681d8fb372bbc3e5322a1483dc220dbdb
Neves Tuan <neves.pham@gmail.com>
2024-07-19 07:25:28 +1000

    Version: daf9e3d889441183dd7c3d00179f03f453778cb8

---
+++ prepareBuildUat.sh
@@ -0,0 +1,52 @@
+set -Eeuo pipefail
+git checkout main
+git pull origin main
+
+BUILD_ID_FILE="build/BUILD_ID"
+CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
+if [ -e "$BUILD_ID_FILE" ]; then
+    # Read the stored commit hash from the BUILD_ID file
+    STORED_COMMIT_HASH=$(cat "$BUILD_ID_FILE")
+
+    # Compare the current commit hash with the stored commit hash
+    if [ "$CURRENT_COMMIT_HASH" == "$STORED_COMMIT_HASH" ]; then
+        echo "Current commit hash matches the stored commit hash. Exiting..."
+    else
+        echo "Current commit hash does not match the stored commit hash. Running 'yarn build'..."
+        yarn build
+    fi
+else
+    echo "BUILD_ID file not found. Running 'yarn build'..."
+    yarn build
+fi
+
+echo "$CURRENT_COMMIT_HASH" > "$BUILD_ID_FILE"
+echo "Build process completed."
+
+
+BUILD_FOLDER_PATH=../absolute-api
+
+if [ ! -d "$BUILD_FOLDER_PATH" ]; then 
+    git clone https://github.com/Absolute-PA/absolute-api.git $BUILD_FOLDER_PATH
+fi
+
+
+cp -r ./build $BUILD_FOLDER_PATH/
+cp ./package.json $BUILD_FOLDER_PATH/
+cp ./yarn.lock $BUILD_FOLDER_PATH/
+cp -r ./certificates $BUILD_FOLDER_PATH/
+cp -r ./assets $BUILD_FOLDER_PATH/
+cp ./.env.prod $BUILD_FOLDER_PATH/
+cp -r ./scripts/ $BUILD_FOLDER_PATH/
+cp ./newrelic.js $BUILD_FOLDER_PATH/
+
+
+cd $BUILD_FOLDER_PATH
+echo "\nnpm_package_version=$CURRENT_COMMIT_HASH" >> .env.prod
+git reset --hard
+git fetch
+git checkout uat
+git pull origin uat
+git add .
+git commit -m "Version: $CURRENT_COMMIT_HASH"
+git push
---
+++ startApiUat.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Checkout to the main branch and reset to the latest from origin
+git checkout uat
+git fetch origin
+git reset --hard origin/uat
+
+# Install dependencies
+yarn
+
+# Start the database
+./startDB.sh
+
+# Check if .env file exists
+if test -f .env; then 
+    # Extract npm_package_version line from .env.prod
+    npm_package_version=$(grep 'npm_package_version' .env.prod)
+    
+    # Create a temporary file with updated content
+    awk -v new_line="$npm_package_version" '
+    BEGIN { found = 0 }
+    {
+        if ($0 ~ /^npm_package_version=/) {
+            print new_line
+            found = 1
+        } else {
+            print
+        }
+    }
+    END {
+        if (!found) {
+            print new_line
+        }
+    }' .env > .env.tmp
+    
+    # Move the temporary file to .env
+    mv .env.tmp .env
+else
+    # If .env does not exist, copy .env.prod to .env
+    cp .env.prod .env
+fi
+
+# Start the server with HTTPS enabled
+HTTPS=true node -r newrelic ./build/index.js
